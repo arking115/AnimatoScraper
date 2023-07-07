0c82dc2db5f56163a6bc5750cfdc6605ebb65514
arking115 <rusalexandru115@gmail.com>
2023-07-07 15:16:46 +0300

    Fixed venv mistake

--- generate_graph.py
+++ generate_graph.py
@@ -3,16 +3,13 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 from matplotlib import backend_bases
 
-# Load the Excel file with 83 sheets
 excel_file = 'Animato Analysis.xlsx'
 data_frames = pd.read_excel(excel_file, sheet_name=None)
 
-# Define the video count categories
 categories = ['1-5 Videos', '6-15 Videos', '15+ Videos']
 
 # Iterate over each category and plot the data if it exists
 for i, category in enumerate(categories):
-    # Filter the data based on the video count category
     if i == 0:
         filtered_data = {
             sheet_name: df for sheet_name, df in data_frames.items() if 0 <= len(df) <= 5
@@ -28,7 +25,6 @@ for i, category in enumerate(categories):
     else:
         filtered_data = {}
 
-    # Check if there is data available for the category
     if filtered_data:
         # Exclude line graphs from other categories
         if i == 0:
@@ -45,11 +41,10 @@ for i, category in enumerate(categories):
 
         # Iterate over each sheet in the filtered data and plot the data
         for sheet_name, df in filtered_data.items():
-            video_number = [0] + list(range(1, len(df) + 1))  # Add 0 to the beginning of the list
-            view_count = [0] + df.iloc[:, 1].tolist()  # Add 0 to the beginning of the list
+            video_number = [0] + list(range(1, len(df) + 1))
+            view_count = [0] + df.iloc[:, 1].tolist()
             ax.plot(video_number, view_count, label=sheet_name)
 
-        # Set the labels and title for the current plot
         ax.set_xlabel('Video Number')
         ax.set_ylabel('View Count')
         ax.set_title(category)
@@ -58,11 +53,9 @@ for i, category in enumerate(categories):
         # Format the y-axis labels
         ax.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: f'{int(x):,}'))
 
-        # Adjust the x-axis limits for the first plot
         if i == 0:
             ax.set_xlim(0, 5)
         elif i == 1:
             ax.set_xlim(0, 15)
 
-        # Display the plot
         plt.show()
